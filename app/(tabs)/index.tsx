928cf4af84bb3677a586b82c1e072dfa1d89914f
swopnilll <swopnilacharya@gmail.com>
2025-03-25 00:43:56 +1100

    Combining multiple styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
 import { View, StyleSheet } from "react-native";
 
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
-const PlaceHolderImage = require("@/assets/images/background-image.png");
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceHolderImage} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -20,8 +25,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     alignItems: "center",
   },
-
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
